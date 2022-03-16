84156d27b4866ad114561c902f855246b4015a35
Abhishek Kumar <80601500+itsAbhishek121@users.noreply.github.com>
2022-03-17 01:34:47 +0530

    Added Page 8

---
+++ src/Page_8.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Page_8(){
+    return(
+        <div className="container_8">
+            <p id= "p28">With Diana, you’re in control of your health, your every day, and your story.</p>
+        </div>
+    )
+}
+export default Page_8;
