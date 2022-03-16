50e1c008ab7f2116f876aa8fc952875ce492b9b8
Abhishek Kumar <80601500+itsAbhishek121@users.noreply.github.com>
2022-03-17 01:33:21 +0530

    Added Page 4 and 5

---
+++ src/Page_4.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Page_4(){
+    return(
+        <div className="container_4">
+            <p id= "p20">Track your mood</p>
+            <p id= "p21">All days and all moods are not made equal. But we can only improve what we measure. Track your mood and vitals (including skin and hair health) so you can take better care of yourself every day. </p>
+            <button id= "b2">Track you mood on the app</button>
+            <div id= "subcontainer_5">
+                <img id= "img3" src={require('C:/Users/Asus/Desktop/Dyeus/task/src/images/i3.jpg')}/>
+                <img id= "img4" src={require('C:/Users/Asus/Desktop/Dyeus/task/src/images/i4.jpg')}/>
+                <img id= "img5" src={require('C:/Users/Asus/Desktop/Dyeus/task/src/images/i5.jpg')}/>
+                <img id= "img6" src={require('C:/Users/Asus/Desktop/Dyeus/task/src/images/i6.jpg')}/>
+            </div>
+        </div>
+    )
+}
+export default Page_4;
---
+++ src/Page_5.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Page_5(){
+    return(
+        <div className="container_5">
+            <p id= "p22">Track your period</p>
+            <p id= "p23">Stay in the know with reliable AI-based and science-backed period, ovulation, and PMS predictions. The intuitive design makes tracking your cycle effortless and helps you stay on top of your monthly data. </p>
+            <button id= "b3">Track you period on the app</button>
+            <img id= "img7" src={require('C:/Users/Asus/Desktop/Dyeus/task/src/images/i7.jpg')}/>
+        </div>
+    )
+}
+export default Page_5
