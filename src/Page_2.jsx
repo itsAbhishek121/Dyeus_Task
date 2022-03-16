961246be312fd238d5f96e394b3face69f4b463a
Abhishek Kumar <80601500+itsAbhishek121@users.noreply.github.com>
2022-03-17 01:28:38 +0530

    Added Page 2

---
+++ src/Page_2.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Page_2(){
+    return(
+        <div className="container_2">
+            <p id= "p8">What makes us different?</p>
+            <p id= "p9">Reimagining and rewiring self-care</p>
+            <div id="subcontainer_2">
+                <p id= "p10">You care about conscious consumption, and so do we.</p>
+                <p id= "p11">When it comes to your health, “good enough” is not good enough. We’re here to flip the script on that narrative because you deserve the best.</p>
+                <p id= "p12">We collaborate with researchers and doctors to:</p>
+                <ul id= "p13">
+                    <li>Help you get granular about your health and self-care with personalized insights, and</li><br/>
+                    <li>Equip you with simple, sustainable products that get the job done</li>
+                </ul>
+                <p id= "p14">No to-the-moon-and-back claims in this neck of the woods.</p>
+                <p id= "p15">We’re just solving real problems for real people here.</p>
+            </div>
+        </div>
+    )
+}
+export default Page_2;
