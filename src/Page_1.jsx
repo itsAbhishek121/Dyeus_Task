a6858df6c3dec87a32e070fd361ef462d239ba45
Abhishek Kumar <80601500+itsAbhishek121@users.noreply.github.com>
2022-03-17 05:47:45 +0530

    Updated Page 1

--- src/Page_1.jsx
+++ src/Page_1.jsx
@@ -8,7 +8,7 @@ function Page_1(){
             <p id= "t1">About Diana</p>
             <p id= "st1">A platform that looks out for you</p>
             <p id= "p1">Diana empowers you to understand how your body works so you can look and feel your best.</p>
-            <hr style={{width:"297px",margin: "26px 0 0 46px", border: "1px solid #000000"}}/>
+            <hr style={{width:"297px", border: "0.1px solid #000000"}}/>
             <p id= "t2">Our Philosophy</p>
             <p id= "p2">Sustainable wellness is a big-picture, inside out approach</p>
             <p id= "p3">Diana is on a mission to create thoughtful, science-backed solutions for women who want to make informed decisions about their health and wellness.<br/><br/>That’s why we’ve created an integrated ecosystem of:</p>
